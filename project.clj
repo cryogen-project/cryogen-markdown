44b7fef0cfc1de88963e3ab30fb213230a720ce0
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2015-08-30 10:55:52 -0400

    bumped up clj version

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/cryogen-project/cryogen-markdown"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [cryogen-core "0.1.16"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [cryogen-core "0.1.25"]
                  [markdown-clj "0.9.69"]])
