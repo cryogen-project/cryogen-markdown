62e389a997673c62b21a37cb65073dec338e0959
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2015-08-30 10:53:38 -0400

    bumped up markdown-clj

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [cryogen-core "0.1.16"]
-                 [markdown-clj "0.9.63"]])
+                 [markdown-clj "0.9.69"]])
