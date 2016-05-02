78d61dd1faa93ecaa6adcff8c0120f0a1fbc20a9
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-05-02 00:41:34 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject cryogen-markdown "0.1.3"
+(defproject cryogen-markdown "0.1.4"
   :description "Markdown parser for Cryogen"
   :url "https://github.com/cryogen-project/cryogen-markdown"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [cryogen-core "0.1.26"]
-                 [markdown-clj "0.9.88"]])
+                 [markdown-clj "0.9.89"]])
