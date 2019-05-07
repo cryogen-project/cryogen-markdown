caa4ae787c4abcaba102dc9eb0fb045d23c4c6cd
Dominic Freeston <dominicfreeston@users.noreply.github.com>
2019-05-07 22:22:53 +0100

    Update markdown-clj version and bump version

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject cryogen-markdown "0.1.9"
+(defproject cryogen-markdown "0.1.10"
   :description "Markdown parser for Cryogen"
   :url "https://github.com/cryogen-project/cryogen-markdown"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [cryogen-core "0.1.67"]
-                 [markdown-clj "1.0.8"]])
+                 [markdown-clj "1.0.9"]])
