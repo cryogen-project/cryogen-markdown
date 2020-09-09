d7b2d3517c00e9cce62ba29b799800fd24905c5a
Dorab Patel <dorabpatel@gmail.com>
2020-09-09 15:56:02 -0700

    Allow multiple extensions in  Markup.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject cryogen-markdown "0.1.11"
+(defproject cryogen-markdown "0.1.12"
   :description "Markdown parser for Cryogen"
   :url "https://github.com/cryogen-project/cryogen-markdown"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [cryogen-core "0.1.67"]
+                 [cryogen-core "0.3.2"]
                  [markdown-clj "1.10.0"]])
